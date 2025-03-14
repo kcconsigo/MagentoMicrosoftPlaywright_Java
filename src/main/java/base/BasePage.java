63d99a12daf0a72a147e054300a9668faeb2310d
kcconsigo <kcconsigo@gmail.com>
2025-03-14 13:10:03 +0800

    Create Base Page java class under src main folder

---
+++ src/main/java/base/BasePage.java
@@ -0,0 +1,25 @@
+package base;
+
+import com.microsoft.playwright.Page;
+
+public class BasePage {
+	
+	public Page page;
+	
+	public BasePage(Page page) {
+		
+		this.page = page;
+		
+	}
+	
+	public void click(String locatorKey) {
+		
+		try {
+			
+		}catch(Exception e) {
+			
+		}
+		
+	}
+
+}
