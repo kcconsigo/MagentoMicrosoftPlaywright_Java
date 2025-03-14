98e478080c57c159fe550dbd484266014bb4fbdb
kcconsigo <kcconsigo@gmail.com>
2025-03-14 14:46:53 +0800

    Modified: Instantiate Logger for Property java class

--- src/test/java/org/testcases/TestProperties.java
+++ src/test/java/org/testcases/TestProperties.java
@@ -5,21 +5,31 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+
 public class TestProperties {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		
+//		Logger logger =  (Logger) LogManager.getLogger(TestProperties.class);
+		
+		final Logger log = LogManager.getLogger(TestProperties.class);
 		Properties prop = new Properties();
 		try {
 			FileInputStream fis = new FileInputStream("C:/Users/Kc Cabelin/eclipse-workspace/MicrosoftPlaywright/src/test/resources/packages/locators.properties");
 			prop.load(fis);
+			log.info("Properties are loaded");
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
+		
 		System.out.println(prop.get("browser"));
+		log.info("Browser Info has launched");
 		System.out.println(prop.get("username_xPath"));
 		
 
