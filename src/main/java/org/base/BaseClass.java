ec7df8db503aa4831aec73619e12a15889dde344
kcconsigo <kcconsigo@gmail.com>
2025-03-18 15:22:38 +0800

    Feature: Add Home Page Test case 01

---
+++ src/main/java/org/base/BaseClass.java
@@ -0,0 +1,51 @@
+package org.base;
+
+import java.nio.file.Paths;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.Tracing;
+import com.microsoft.playwright.options.LoadState;
+
+public class BaseClass {
+	
+	Playwright playwright;
+	protected Page page;
+	Browser browser;
+	BrowserContext browserContext;
+	
+	@Parameters({"appURL", "browserType"})
+	public Page launchBrowser(String appURL, String browserType) {
+		
+		try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50));
+            BrowserContext browsercontext = browser.newContext(new Browser.NewContextOptions().setViewportSize((int) 1536.0, (int) 864.0));
+            browsercontext.setDefaultTimeout(60000);
+            browsercontext.tracing().start(new Tracing.StartOptions()
+            		.setScreenshots(true)
+            		.setSnapshots(true)
+            		.setSources(true));
+		      Page browserpage = browsercontext.newPage();
+		      browserpage.navigate(appURL);
+		      browsercontext.tracing().stop(new Tracing.StopOptions().setPath(Paths.get("trace.zip")));
+		      browserpage.waitForLoadState(LoadState.DOMCONTENTLOADED);
+		      browserpage.waitForLoadState(LoadState.NETWORKIDLE);
+		    }
+		return page;
+		
+	}
+			
+			public void closeBrowser() {
+				page.close();
+				browser.close();
+				playwright.close();
+	}
+
+}
