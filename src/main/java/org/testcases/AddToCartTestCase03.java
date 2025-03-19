d61a95df2addda03c1effa3a739ad34dd281e8fb
kcconsigo <kcconsigo@gmail.com>
2025-03-19 13:56:16 +0800

    Modified: Update autoamtion scripts for Add To Cart Page java main class

--- src/main/java/org/testcases/AddToCartTestCase03.java
+++ src/main/java/org/testcases/AddToCartTestCase03.java
@@ -7,11 +7,14 @@ import java.nio.file.Paths;
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserContext;
 import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 import com.microsoft.playwright.Tracing;
 import com.microsoft.playwright.options.AriaRole;
 import com.microsoft.playwright.options.LoadState;
+
+import pages.AddToCartPage;
 
 public class AddToCartTestCase03 {
 	
@@ -30,6 +33,7 @@ public class AddToCartTestCase03 {
 		try (Playwright playwright = Playwright.create()) {
             Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50));
             BrowserContext browsercontext = browser.newContext(new Browser.NewContextOptions().setViewportSize((int)width,(int) height));
+            browsercontext.setDefaultTimeout(60000);
             browsercontext.tracing().start(new Tracing.StartOptions()
             		.setScreenshots(true)
             		.setSnapshots(true)
@@ -38,34 +42,15 @@ public class AddToCartTestCase03 {
             page.navigate("https://magento.softwaretestingboard.com/");
             browsercontext.tracing().stop(new Tracing.StopOptions().setPath(Paths.get("trace.zip")));
             System.out.println(page.title());
-		      page.navigate("https://magento.softwaretestingboard.com/");
-		      page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Sign In")).click();
-		      page.getByLabel("Email", new Page.GetByLabelOptions().setExact(true)).click();
-		      page.getByLabel("Email", new Page.GetByLabelOptions().setExact(true)).fill("bobby.schneider@gmail.com");
-		      page.getByLabel("Password").click();
-		      page.getByLabel("Password").fill("4skYbT@!hkh6yCi");
-		      page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Sign In")).click();
-		      page.waitForLoadState(LoadState.LOAD);
-		      page.waitForLoadState(LoadState.NETWORKIDLE);
-		      page.getByRole(AriaRole.MENUITEM, new Page.GetByRoleOptions().setName("Pants")).click();
-		      page.getByText("Aether Gym Pant").click();
-		      page.getByLabel("33").click();
-		      page.getByLabel("32").click();
-		      page.getByLabel("33").click();
-		      page.getByLabel("Blue").click();
-		      page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Add to Cart")).click();
-		      page.getByText("You added Aether Gym Pant to").click();
-		      page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("shopping cart")).click();
-		      page.locator("#shopping-cart-table").getByTitle("Aether Gym Pant ").click();
-		      page.navigate("https://magento.softwaretestingboard.com/checkout/cart/");
-		      page.getByRole(AriaRole.ROW, new Page.GetByRoleOptions().setName("Aether Gym Pant Aether Gym")).locator("dl").click();
-		      page.getByText("$59.20").nth(1).click();
-		      page.getByText("$59.20").nth(2).click();
-		      page.getByRole(AriaRole.ROW, new Page.GetByRoleOptions().setName("Order Total $")).locator("span").click();
-		      page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Proceed to Checkout")).click();
-		      page.getByRole(AriaRole.IMG, new Page.GetByRoleOptions().setName("Loading...")).click();
+            
+            AddToCartPage addtoCartPage = new AddToCartPage(page);
+            addtoCartPage.addtocutSigninLink();
+            addtoCartPage.addtocustLoginEmail(null);
+            addtoCartPage.addtocustLoginPassword(null);
+            addtoCartPage.addtocustLoginButton();
+            addtoCartPage.addtocustitemlistPage();
+            addtoCartPage.addtocartitemlistPage();
 		    }
-
 	}
 
 }
